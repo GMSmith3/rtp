0a52e68032e834aad0cdb12dd3db5a27cfb1b6bb
George M. Smith III <45109996+GMSmith3@users.noreply.github.com>
2020-12-01 11:45:06 -0500

    Stewardship Level Assignment Tool - Update
    ~Drop-down menu now changes the interactive table view based on donor level
    ~Download button now properly saves output table as .csv
    ~reconfigured with 2 new reactive expressions to achieve these ends

--- app.R
+++ app.R
@@ -1,3 +1,6 @@
+# Load R packages
+library(shiny)
+
 #App to upload annual fund donor .csv and assign stewardship donor levels by account
 ui <- fluidPage(
   # App title ----
@@ -17,22 +20,38 @@ ui <- fluidPage(
                 buttonLabel = "Browse Files",
                 placeholder = "Awaiting .csv Selection"),
       hr(),
+      selectInput("subsets",
+                  label = h3("Select Stewardship Level to View:"),
+                  choices = c("All Donor Levels" = "All Donor Levels",
+                              "Supporters" = "Supporters",
+                              "Investors" = "Investors",
+                              "Underwriters" = "Underwriters",
+                              "Performers" = "Performers",
+                              "Directors" = "Directors",
+                              "Producers" = "Producers"),
+                  selected = 1,
+                  multiple = FALSE),
+      hr(),
+      #Output: Data file ---
+      DTOutput("file"),
     ),
-      #Main panel for displaying outputs ---
-      mainPanel(
-        
-        #Output: Data file ---
-        DTOutput("files"),
-        
-        #Output: processed data file ---
-        DTOutput("head")
-      )
+    #Main panel for displaying outputs ---
+    mainPanel(
+      
+      #Add Download Button ---
+      downloadButton('downloadData', "Download"),
+      hr(),
+      
+      #Output: processed data file ---
+      DTOutput("donorLevels")
+    )
   )  
 )
-server <- function(input, output) {
-  output$files <- renderDT({
-    req(input$upload)
-    Data <- fread(input$upload$datapath)
+server <- function(input, output){
+  levelsAssigned <- reactive({
+    Data <- input$upload
+    req(Data)
+    Data <- fread(Data$datapath)
     Data$CloseDate<-as.Date(Data$CloseDate, format = "%m/%d/%y")
     #r convert appropriate columns to factor
     Data[,c(2:4,9,15:17,19:22)] <- lapply((Data[,c(2:4,9,15:17,19:22)]), function(x) {
@@ -59,7 +78,7 @@ server <- function(input, output) {
                       (Stage != "Refunded") &
                       (PaymentType != "Ticket Order Refund")),]
     #Filter for giving period (if not imported that way):
-    donors <- donors[CloseDate >= "2019-07-01" & CloseDate <= "2020-09-15"]
+    donors <- donors[CloseDate >= "2019-07-01" & CloseDate <= Sys.Date()]
     #Calculate the sum of Amount column for every group in AccountID column - (remove NAs from the calculations). Call the new data - DonationTotalsDT:
     DonationTotalsDT <- donors[,.(AccountName, InformalSalutation, MailingStreet, MailingCity, MailingState, MailingZip, DonationTotal = sum(Amount, na.rm = TRUE), CloseDate),by=AccountID]
     #Let's sort the list by AccountID to see how prevalent duplicate records are:
@@ -88,39 +107,40 @@ server <- function(input, output) {
                                                            fifelse(DonationTotal>=2500 & DonationTotal<5000,
                                                                    "Directors",
                                                                    "Producers")))))]
-    
-    #################################################
-    ##Segment donors into stewardship program levels
-    #################################################
-    #Show me the records with donation totals between 125 and 249:
-    Supporters <- Sorted[DonationTotal<250,]
-    #Show me the records with donation totals between 250 and 499:
-    Investors <- Sorted[DonationTotal>=250 & DonationTotal<500,]
-    #Show me the records with donation totals between 500 and 999:
-    Underwriters <- Sorted[DonationTotal>=500 & DonationTotal<1000,]
-    #Show me the records with donation totals between 1000 and 2499:
-    Performers <- Sorted[DonationTotal>=1000 & DonationTotal<2500,] 
-    #Show me the records with donation totals between 2500 and 4999:
-    Directors <- Sorted[DonationTotal>=2500 & DonationTotal<5000,]
-    #Show me the records with donation totals 5000 and up:
-    Producers <- Sorted[DonationTotal>=5000,]
-    #Alphabetize donors by last name within each group (for playbill listing):
-    Supporters <- Supporters %>%
-      arrange(gsub(".*\\s", "", AccountName))
-    Investors <- Investors %>% 
-      arrange(gsub(".*\\s", "", AccountName))
-    Underwriters <- Underwriters %>% 
-      arrange(gsub(".*\\s", "", AccountName))
-    Performers <- Performers %>% 
-      arrange(gsub(".*\\s", "", AccountName))
-    Directors <- Directors %>% 
-      arrange(gsub(".*\\s", "", AccountName))
-    Producers <- Producers %>% 
-      arrange(gsub(".*\\s", "", AccountName))
-    
     return(Sorted)
+    })
+  levelDTs <- reactive({
+    data <- levelsAssigned()
+    level <- input$subsets
+    
+    if (level == "Supporters") {
+      return(data[data$DonorLevel == "Supporters",])
+    } else if (level == "Investors") {
+      return(data[data$DonorLevel == "Investors",])
+    } else if (level == "Underwriters") {
+      return(data[data$DonorLevel == "Underwriters",])
+    } else if (level == "Performers") {
+      return(data[data$DonorLevel == "Performers",])
+    } else if (level == "Directors") {
+      return(data[data$DonorLevel == "Directors",])
+    } else if (level == "Producers") {
+      return(data[data$DonorLevel == "Producers",])
+    } else {
+      return(data)
+    }
   })
-
+  output$donorLevels <- renderDT({
+    levelDTs()
+  })
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$subsets, input$filetype, sep=".")
+    },
+    content = function(file) {
+      write.csv(levelDTs(), file, row.names = FALSE)
+    }
+  )
 }
+
 
 shinyApp(ui = ui, server = server)
